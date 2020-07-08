420e349ab3b7e8ea7af2e81eaf7b06d055ec37e4
vh13294 <huotvongpanha@gmail.com>
2020-07-08 11:34:30 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  initSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
+
+function initSwagger(app: INestApplication) {
+  const options = new DocumentBuilder()
+    .setTitle('Seng Kea API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('apis')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+}
