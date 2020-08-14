c24c11bca529af04f8577fc231cfb9fd1c64245d
vh13294 <huotvongpanha@gmail.com>
2020-08-14 20:04:46 +0700

    organise folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { InventoriesModule } from './inventories/inventories.module';
 // import typeORMConfig from './config/database.config';
 // import { TypeOrmModule } from '@nestjs/typeorm';
 import { PrismaModule } from './prisma/prisma.module';
+import { InventoriesModule } from './controllers/inventories/inventories.module';
 
 @Module({
   imports: [
