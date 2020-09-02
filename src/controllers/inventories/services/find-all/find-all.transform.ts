7641bca9cff30cbe4a4df47071b9f19cd4b78160
vh13294 <huotvongpanha@gmail.com>
2020-09-02 17:50:10 +0700

    update npm

--- src/controllers/inventories/services/find-all/find-all.transform.ts
+++ src/controllers/inventories/services/find-all/find-all.transform.ts
@@ -2,16 +2,17 @@ import { Inventory } from "@prisma/client";
 
 export type responseInventory = {
     comment?: string
-    createdAt: Date
     createdBy?: number
     customerId?: number
     date?: Date
     deletedAt?: Date
-    id: number
     locationId?: number
     productId?: number
-    quantity: number
     supplierId?: number
+
+    id: number
+    quantity: number
+    createdAt: Date
     updatedAt: Date
 }
 
