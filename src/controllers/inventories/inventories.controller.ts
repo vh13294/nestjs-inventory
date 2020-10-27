0f5f2ad98e51701f448f88636ef974dc06fe1692
vh13294 <huotvongpanha@gmail.com>
2020-10-27 16:42:22 +0700

    update

--- src/controllers/inventories/inventories.controller.ts
+++ src/controllers/inventories/inventories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query } from '@nestjs/common';
 
 import { CreateInventoryDto } from './services/create-inventory/create-inventory.dto';
 import { FindSingleLocationInventoryDto } from './services/find-single-location/find-single-location-inventory.dto';
@@ -7,7 +7,6 @@ import { CreateInventoryService } from './services/create-inventory/create-inven
 import { FindAllService } from './services/find-all/find-all.service';
 
 import { SignedUrlService } from 'nestjs-signed-url';
-import { SignedUrlGuard } from 'nestjs-signed-url';
 
 @Controller('inventories')
 export class InventoriesController {
@@ -35,27 +34,5 @@ export class InventoriesController {
         @Query() findSingleLocationInventoryDto: FindSingleLocationInventoryDto
     ): ReturnType<FindSingleLocationService['findSingleLocation']> {
         return this.findSingleLocationService.findSingleLocation(findSingleLocationInventoryDto);
-    }
-
-    @Get('emailVerification')
-    @UseGuards(SignedUrlGuard)
-    async emailVerification(): Promise<string> {
-        return '123'
-    }
-
-    @Get('testSignedUrl')
-    async testSignedUrl(): Promise<string> {
-        const params = {
-            id: 1,
-            reset: false,
-            // signed: 'test'
-        }
-        const signedUrl = this.signedUrlService.signedControllerRoute(
-            InventoriesController,
-            InventoriesController.prototype.emailVerification,
-            new Date('2021-12-12'),
-            params
-        )
-        return signedUrl
     }
 }
