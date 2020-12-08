51df63468e8736422d643b821973ec425661f3db
vh13294 <huotvongpanha@gmail.com>
2020-12-08 15:49:36 +0700

    refactor pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   apps: [{
     name: "nest",
-    script: "node -r dotenv/config dist/src/main.js",
+    script: "dist/src/main.js",
+    // args: [],
+    // node_args: "-r dotenv/config",
     instances: "max",
     exec_mode: "cluster",
     max_memory_restart: "300M",
-    error_file: "log/err.log",
-    out_file: "log/out.log",
+    error_file: "/dev/null",
+    out_file: "/dev/null",
     log_file: "log/combined.log",
     merge_logs: true,
     time: true
