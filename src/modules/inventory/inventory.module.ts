0ef6e7d5744f1dc56d2155dfeca7a796023f0c8d
vh13294 <huotvongpanha@gmail.com>
2020-12-08 16:23:42 +0700

    make PrismaService global

--- src/modules/inventory/inventory.module.ts
+++ src/modules/inventory/inventory.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { InventoryController } from './inventory.controller';
 
-import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateInventoryService } from './services/create-inventory/create-inventory.service';
 import { FindSingleLocationService } from './services/find-single-location/find-single-location.service';
 import { FindAllService } from './services/find-all/find-all.service';
@@ -10,7 +9,6 @@ import { FindAllService } from './services/find-all/find-all.service';
 @Module({
   controllers: [InventoryController],
   providers: [
-    PrismaService,
     FindAllService,
     CreateInventoryService,
     FindSingleLocationService,
